8c1051aea5086888ec92a07327621f434143302f
sarkaromar <ausantemonir@gmail.com>
2017-10-09 17:23:00 +0600

    Theme update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
+// main require var
 var gulp = require('gulp');
+// others require plugins
 var sass = require('gulp-sass'),
     watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    gulpIf = require('gulp-if');
+
+    var paths = {
+      html: "src/**/*.html",
+      css: "src/**/*.css"
+    };
+
+// task start
 
     gulp.task('browserSync', function() {
       browserSync.init({
@@ -19,11 +31,18 @@ var sass = require('gulp-sass'),
           stream: true
         }))
     });
-    gulp.task('watch', ['browserSync'], function (){
-      gulp.watch('app/sass/*.scss', ['sass']); 
-    })
+
     gulp.task('watch:sass', function () {
       gulp.watch('app/sass/*.scss', ['sass']); 
     });
 
-    gulp.task('default', ['sass', 'watch:sass', 'browserSync']);
+    gulp.task('useref', function(){
+      return gulp.src('app/*.html')
+        .pipe(useref())
+        // Minifies only if it's a JavaScript file
+        .pipe(gulpIf('app/**/*.js', uglify()))
+        .pipe(gulp.dest('dist'))
+    });
+
+// default task
+gulp.task('default', ['sass', 'watch:sass', 'useref', 'browserSync']);
